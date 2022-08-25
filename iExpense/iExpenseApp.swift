c99d0c5581cc21605a21763b4e99471235312984
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-08-25 22:47:44 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Николай Никитин on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
